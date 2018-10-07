d4f66ec1b4d42dbbe28dd2287693a357d24d7c79
Liron <Liron.Blum@BankLeumi.co.il>
2018-10-07 09:10:49 +0300

    Dijkstra algorithm only

---
+++ graphs/dijkstraFuncs.js
@@ -0,0 +1,53 @@
+const { reduce } = require(`lodash`);
+
+//neighbours = list of vertacies that are connected to minDistVertex.
+//dist is the Edge value between minDistVertex and his neighbour
+class Dijkstra {
+    constructor(graph, start, finish) {
+      this.graph = graph
+      this.start = start
+      this.finish = finish
+      this.unVIsited = {}
+    }
+    
+    algorithm(){
+         //init
+        for (const [key, value] of Object.entries( this.graph)) {
+            key === this.start ? value.dist = 0 : value.dist = Number.POSITIVE_INFINITY
+            this.unVIsited[key] = value.dist
+            value.parent = null  
+        }
+
+        while(Object.keys(this.unVIsited).length > 0){ //loop until all vertex are visited
+
+            const minDistVertex = Object.keys(this.unVIsited).reduce( (a,b) => this.graph[a].dist < this.graph[b].dist ? a : b ); 
+            delete this.unVIsited[minDistVertex];
+            
+            
+            for (const [vertex, edgeDist] of Object.entries(this.graph[minDistVertex].links)) { //go through each neighbour
+
+                if(this.unVIsited.hasOwnProperty(vertex)){ //verify that the neighbour is visitable
+                    const newDist = this.graph[minDistVertex].dist + edgeDist  
+
+                    if(newDist < this.graph[vertex].dist) {
+                        this.graph[vertex].dist = newDist //assign neighbour's new calculated dist if smaller then his original dist
+                        this.graph[vertex].parent = minDistVertex 
+                    }
+                }   
+            }
+        }
+    }
+    
+    findPath() {
+     
+        this.algorithm()
+
+        //TODO recursively return path and path length
+        console.log('******************')
+        console.log(this.graph)
+    }
+}
+
+module.exports = Dijkstra
+
+
